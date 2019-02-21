b4b72944e2b019af84a08626317844252c09e6d1
adz.macb003 <adz.macb003@MacBook-Air-de-adzmacb003.local>
2019-02-21 12:30:24 -0300

    finish

---
+++ app.js
@@ -0,0 +1,29 @@
+const github = new GitHub();
+const ui = new UI();
+
+// Search input
+const searchUser = document.getElementById('searchUser');
+
+// Search input event listener
+searchUser.addEventListener('keyup',(e) => {
+  // Get input text
+  const userText = e.target.value;
+
+  if(userText !== '') {
+    // Make http call
+    github.getUser(userText)
+      .then(data =>  {
+        if(data.profile.message == 'Not Found' ) {
+          // Show alert
+          ui.showAlert('User not found.', 'alert alert-danger');
+        } else {
+          // Show the profile
+          ui.showProfile(data.profile);
+          ui.showRepos(data.repos);
+        }
+      })
+  } else {
+    // Clear profile
+    ui.clearProfile();
+  }
+});
---
+++ github.js
@@ -0,0 +1,23 @@
+
+class GitHub {
+  constructor() {
+    this.client_id = 'ed57d54759c913fb8f29';
+    this.client_secret = 'b3aea7e2e58641da594ed43cd06223151dcaa2a2';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await reposResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
